e819f933029be873792bbc4687052b7f5c72d4f8
Yuvraj <ducatimonster729@gmail.com>
2025-06-29 19:36:04 +0530

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import './globals.css'
 
 import Footer from '@/components/Footer/Footer'
 import Navbar from '@/components/Navbar/Navbar'
-import ThemeMenu from '@/components/Theme/ThemeMenu'
 import { Fira_Code } from 'next/font/google'
 
 const firaCode = Fira_Code({ subsets: ['latin'], weight: ['300', '400', '500', '600', '700'] })
@@ -38,7 +37,6 @@ export default function RootLayout({
           <Navbar />
         </header>
         {children}
-        <ThemeMenu />
         <Footer />
       </body>
     </html>
